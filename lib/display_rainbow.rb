9edb5d0aaf8957c9cfcdbf8a1db2d0fc341abe1c
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-14 02:01:36 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,7 @@ def display_rainbow(colors)
   result = ""
   while colors.length > 0
     color = colors.shift
-    result += "#{color[0].upcase}: #{color}"
+    result += "#{colors[0][0].upcase}: #{color}"
     if colors.length > 0
       result += ", "
     else
@@ -11,7 +11,7 @@ def display_rainbow(colors)
     end
   end
   puts result
-  puts = "#{colors[0]}, #{colors[1]}#{colors}#{colors[3]}#{colors[4]}#{colors[5]}#{colors[6]}"
+  puts = "#{colors[0][0].upcase}: #{colors[0]}, #{colors[1][0].upcase}: #{colors[1]}, #{colors[2][0].upcase}: #{colors}, #{colors[3][0].upcase}: #{colors[3]}, #{colors[4][0].upcase}: #{colors[4]}, #{colors[5][0].upcase}: #{colors[5]}, #{colors[6][0].upcase}: #{colors[6]}\n"
 end
 
 #  just_for_the_intrusive_test
