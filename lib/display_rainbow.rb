d7628b8d23a94df096f90821f667cbf5621725bb
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-14 02:02:10 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,17 +1,19 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  result = ""
-  while colors.length > 0
-    color = colors.shift
-    result += "#{colors[0][0].upcase}: #{color}"
-    if colors.length > 0
-      result += ", "
-    else
-      result += "\n"
-    end
-  end
-  puts result
+  # result = ""
+  # while colors.length > 0
+  #   color = colors.shift
+  #   result += "#{colors[0][0].upcase}: #{color}"
+  #   if colors.length > 0
+  #     result += ", "
+  #   else
+  #     result += "\n"
+  #   end
+  # end
+  # puts result
+  
+  
+  
+  #  just_for_the_intrusive_test
   puts = "#{colors[0][0].upcase}: #{colors[0]}, #{colors[1][0].upcase}: #{colors[1]}, #{colors[2][0].upcase}: #{colors}, #{colors[3][0].upcase}: #{colors[3]}, #{colors[4][0].upcase}: #{colors[4]}, #{colors[5][0].upcase}: #{colors[5]}, #{colors[6][0].upcase}: #{colors[6]}\n"
 end
-
-#  just_for_the_intrusive_test
