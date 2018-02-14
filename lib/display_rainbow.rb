e4122f1adc24fefcfbed104f19995fcef0bab01d
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-14 01:45:22 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  result = ""
+  while colors.length > 0
+    color = colors.shift
+    result += "#{color[0].upcase}: #{color}"
+    if colors.length > 0
+      result += ", "
+    else
+      result += "\n"
+    end
+  end
+  puts result
+end
